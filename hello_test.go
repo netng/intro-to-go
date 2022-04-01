65048b3ce49d8df894a72f16d5bd169e8d3e8161
nan <nandangsopyan08@gmail.com>
2022-04-01 16:53:30 +0700

    updated test for hello.go

--- hello_test.go
+++ hello_test.go
@@ -7,11 +7,22 @@ import "testing"
 // # it should return string "Hello, ${name}"
 
 func TestHello(t *testing.T) {
-	got := Hello("asep")
-	want := "Hello, asep"
-
-	if got != want {
-		t.Errorf("\n got: %q\n want: %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got: %q, want: %q", got, want)
+		}
 	}
-}
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Asep")
+		want := "Hello, Asep"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
