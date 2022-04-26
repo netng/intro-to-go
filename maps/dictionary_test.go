b5e8b9f5411c4c952c41216a3bbba09eb668627f
netng <net.nandang@gmail.com>
2022-04-26 10:09:37 +0700

    lear about maps and its testing

---
+++ maps/dictionary_test.go
@@ -0,0 +1,15 @@
+package maps
+
+import "testing"
+
+func TestDictionary(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	if got != want {
+		t.Errorf("got %q, want: %q give, %q", got, want, "test")
+	}
+
+}
